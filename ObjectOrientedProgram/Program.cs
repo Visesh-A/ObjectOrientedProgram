0ba94570147dfc62eea2ccff1b89b94ba3735b15
“Visesh-A” <viseshajl@gmail.com>
2023-03-23 17:44:32 +0530

    [visesh]Add:Added

--- ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ObjectOrientedProgram.InventoryDataManagementProblems;
+using ObjectOrientedProgram.InventoryManagement;
 using System;
 namespace ObjectOrientedProgram
 {
@@ -9,7 +10,7 @@ namespace ObjectOrientedProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Select option to perform \n1.Json Inventory \n2.Exit");
+                Console.WriteLine("Select option to perform \n1.Json Inventory \n2.Inventory Management \n3.Stock Account Program \n4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -18,6 +19,33 @@ namespace ObjectOrientedProgram
                         inventory.ReadJsonfile(@"C:\Users\VISESH\Documents\bl excercise\ObjectOrientedProgram\ObjectOrientedProgram\InventoryDataManagementProblems\Inventory.json");
                         break;
                     case 2:
+                        InventoryDataManagement inventoryDataManagement = new InventoryDataManagement();
+                        bool flag1 = true;
+                        while (flag1)
+                        {
+                            Console.WriteLine("Select option to perform \n1 Json File \n2.Add Inventory \n3.Edit \n4.Delete \n5.Exit");
+                            int option1 = Convert.ToInt32(Console.ReadLine());
+                            switch (option1)
+                            {
+                                case 1:
+                                    inventoryDataManagement.ReadJsonFile(@"C:\Users\VISESH\Documents\bl excercise\ObjectOrientedProgram\ObjectOrientedProgram\InventoryManagement\InventoryDetails.json");
+                                    break;
+                                case 2:
+                                    inventoryDataManagement.AddInventory();
+                                    break;
+                                case 3:
+                                    inventoryDataManagement.Edit();
+                                    break;
+                                case 4:
+                                    inventoryDataManagement.Delete();
+                                    break;
+                                case 5:
+                                    flag1 = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
